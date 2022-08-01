751942cf9f9eee723066d4586460e454775ed2e2
SWAPNIL BAWANKAR <swapnilb6@gmail.com>
2022-08-01 13:47:12 +0530

    Commiting and Pushing Changes to Github

---
+++ WebApp.GitHubCICD_Demo/WebApp.GitHubCICD_Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApp.GitHubCICD_Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
